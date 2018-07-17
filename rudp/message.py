e98e72199d32adec97f9bc676d00f78a89e116e4
Ahmad Anvari <ahmad.anv@gmail.com>
2018-07-17 23:06:14 +0430

    Serializer and Deserializer method implemented

--- rudp/message.py
+++ rudp/message.py
@@ -1,9 +1,10 @@
 import threading
-from const import DATAGRAM_TIMEOUT_SECONDS
+import const
+from util.byte_util import int_to_byte, byte_to_int
 
 
 class Message:
-    def __init__(self, rsocket_ins):
+    def __init__(self):
         self.__sequence_number = 0
         self.__is_fragmented = False
         self.__is_ack = False
@@ -11,12 +12,10 @@ class Message:
         self.__fragmentation_offset = 0
         self.__fragmentation_id = 0
         self.__data = []
-        self.timer_thread = threading.Timer(DATAGRAM_TIMEOUT_SECONDS, self.__ace_receive_timeout)
-        self.__rsocket_ins = rsocket_ins
-
+        self.timer_thread = threading.Timer(const.DATAGRAM_TIMEOUT_SECONDS, self.__ace_receive_timeout)
         self.__is_acked = False
 
-    def __init__(self, rsocket_ins, sequence_number, is_fragmented, is_last_fragment, fragmentation_offset, fragmentation_id):
+    def __init__(self, sequence_number, is_fragmented, is_last_fragment, fragmentation_offset, fragmentation_id):
         self.__sequence_number = sequence_number
         self.__is_fragmented = is_fragmented
         self.__is_ack = False
@@ -24,18 +23,57 @@ class Message:
         self.__fragmentation_offset = fragmentation_offset
         self.__fragmentation_id = fragmentation_id
         self.__data = []
-        self.timer_thread = threading.Timer(DATAGRAM_TIMEOUT_SECONDS, self.__ace_receive_timeout)
-        self.__rsocket_ins = rsocket_ins
-
+        self.timer_thread = threading.Timer(const.DATAGRAM_TIMEOUT_SECONDS, self.__ace_receive_timeout)
         self.__is_acked = False
+
+    def append_data(self, byte):
+        self.__data.append(byte)
 
     @staticmethod
     def serialize(message):
-        return []
+        bytes_arr = []
+
+        bytes_arr += int_to_byte(message.__sequence_number, 4)
+        print(bytes_arr)
+
+        flags_byte = 0
+
+        if message.__is_fragmented:
+            flags_byte += pow(2, const.IS_FRAGMENTED_BIT_FLAG)
+
+        if message.__is_ack:
+            flags_byte += pow(2, const.IS_ACK_BIT_FLAG)
+
+        if message.__is_last_fragment:
+            flags_byte += pow(2, const.IS_LAST_FRAGMENT_BIT_FLAG)
+
+        print(flags_byte)
+        bytes_arr.append(flags_byte)
+        print(bytes_arr)
+
+        bytes_arr += int_to_byte(message.__fragmentation_offset, 8)
+        bytes_arr += int_to_byte(message.__fragmentation_id, 8)
+        bytes_arr += message.__data
+
+        return bytes_arr
 
     @staticmethod
-    def deserialize(*bytes):
-        return []
+    def deserialize(bytes_arr):
+        is_fragmented = ((pow(2, const.IS_FRAGMENTED_BIT_FLAG) & bytes_arr[const.FLAGS_BYTE_INDEX]) == bytes_arr[const.FLAGS_BYTE_INDEX])
+        is_ack = ((pow(2, const.IS_ACK_BIT_FLAG) & bytes_arr[const.IS_ACK_BIT_FLAG]) == bytes_arr[const.IS_ACK_BIT_FLAG])
+        is_last_fragment = ((pow(2, const.IS_LAST_FRAGMENT_BIT_FLAG) & bytes_arr[const.IS_LAST_FRAGMENT_BIT_FLAG]) == bytes_arr[const.IS_LAST_FRAGMENT_BIT_FLAG])
+        message = Message(
+            sequence_number=byte_to_int(
+                bytes_arr[const.SEQUENCE_NUMBER_START_INDEX: const.SEQUENCE_NUMBER_LENGTH + const.SEQUENCE_NUMBER_START_INDEX]),
+            is_fragmented=is_fragmented,
+            is_last_fragment=is_last_fragment,
+            fragmentation_offset=byte_to_int(bytes_arr[const.FRAGMENT_OFFSET_START_INDEX: const.FRAGMENT_OFFSET_START_INDEX + const.FRAGMENT_OFFSET_LENGTH]),
+            fragmentation_id=byte_to_int(bytes_arr[const.FRAGMENTATION_ID_START_INDEX: const.FRAGMENTATION_ID_START_INDEX + const.FRAGMENTATION_ID_LENGTH])
+        )
+
+        message.__is_ack = is_ack
+        message.__data = bytes_arr[const.PAYLOAD_START_INDEX:]
+        return message
 
     def ack_message(self):
         self.__is_acked = True
